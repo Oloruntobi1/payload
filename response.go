5747db782b2bbfa46ac86c7cad316c336dab87bf
Oloruntobi1 <blgntobi@gmail.com>
2021-08-16 13:25:33 +0100

    my new version

--- response.go
+++ response.go
@@ -8,8 +8,24 @@ type WalletPayoutResponse struct {
 	Balance int `json:"balance" example:"1"`
 }
 
+type BanksResponse struct {
+	RequestSuccessful bool `json:"requestSuccessful" example:"true"`
+    ResponseMessage string `json:"responseMessage" example:"success"`
+    ResponseCode string `json:"responseCode" example:"0"`
+	ResponseBody []Banks `json:"responseBody"`
+}
+
+type Banks struct {
+	Name string `json:"name" example:"Access bank"`
+	Code string `json:"code" example:"044"`
+	UssdTemplate string `json:"ussdTemplate" example:"*901*Amount*AccountNumber#"`
+	BaseUssdCode string `json:"baseUssdCode" example:"901#"`
+	TransferUssdTemplate string `json:"transferUssdTemplate" example:"*901*AccountNumber#"`
+}
+
+
 // git commit -a -m "my new version"
 // git push
-// git tag v0.1.3
-// git push -q origin v0.1.3
+// git tag v0.1.4
+// git push -q origin v0.1.4
 
